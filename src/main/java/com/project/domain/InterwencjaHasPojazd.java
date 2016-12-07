3bfa04507ca4a87335bdeff2cfdd171dd4b58c0f
adlerpoland <resepoland@o2.pl>
2016-12-07 19:39:10 +0100

    Add files via upload

--- src/main/java/com/project/domain/InterwencjaHasPojazd.java
+++ src/main/java/com/project/domain/InterwencjaHasPojazd.java
@@ -1,25 +1,25 @@
 package com.project.domain;
 
 public class InterwencjaHasPojazd {
-	private long id_interwencja;
-	private long id_pojazd;
+	private long Interwencja_id;
+	private long Pojazd_id;
 	
 	public InterwencjaHasPojazd(int id_int,int id_poj){
-		this.id_interwencja = id_int;
-		this.id_pojazd = id_poj;
+		this.Interwencja_id = id_int;
+		this.Pojazd_id = id_poj;
 	}
 	
 	public long getId_interwencja() {
-		return id_interwencja;
+		return Interwencja_id;
 	}
 	public void setId_interwencja(long id) {
-		this.id_interwencja = id;
+		this.Interwencja_id = id;
 	}
 	
 	public long getId_pojazd() {
-		return id_pojazd;
+		return Pojazd_id;
 	}
 	public void setId_pojazd(long id) {
-		this.id_pojazd = id;
+		this.Pojazd_id = id;
 	}
 }
